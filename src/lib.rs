cb059cb454319a4df4acd2e7ae95ce68322dc22e
Hsia <15273155904@163.com>
2024-06-24 22:04:47 +0800

    add '~' type

--- src/lib.rs
+++ src/lib.rs
@@ -1,130 +1,30 @@
-use std::{collections::HashMap, str::FromStr};
-
 mod resp;
-
-fn parse_str_to_num<T>(s: &str) -> Result<T, &'static str>
-where
-    T: FromStr,
-{
-    if let Some(n) = s.parse::<T>().ok() {
-        Ok(n)
-    } else {
-        Err("parse str to number failed")
-    }
-}
-
-#[allow(unused)]
-fn parse_content(
-    raw_msg: &str,
-    begin_idx: usize,
-    next_begin_idx: &mut usize,
-) -> Result<resp::Value, &'static str> {    // todo need more specific error info
-    let crlf = "\r\n";
-    if let Some(msg_slice) = raw_msg.get(begin_idx..) {
-        if let Some(body) = msg_slice
-            .find(crlf)
-            .and_then(|i| raw_msg.get(begin_idx..begin_idx + i))
-        {
-            *next_begin_idx = begin_idx + body.len() + crlf.len();
-            if let Some((first_char, rest)) =
-                body.chars().next().map(|c| (c, &body[c.len_utf8()..]))
-            {
-                return match first_char {
-                    '+' => Ok(resp::Value::SimpleStr(rest.to_string())),
-                    '-' => Ok(resp::Value::Error(rest.to_string())),
-                    ':' => {
-                        let n = parse_str_to_num::<i64>(rest)?;
-                        Ok(resp::Value::Integer(n))
-                    }
-                    '#' => match rest {
-                        "t" => Ok(resp::Value::Bool(true)),
-                        "f" => Ok(resp::Value::Bool(false)),
-                        _ => Err("unknow type"),
-                    },
-                    '$' => {
-                        let n = parse_str_to_num::<usize>(rest)?;
-                        if let Some(s) = raw_msg.get(*next_begin_idx..*next_begin_idx + n) {
-                            *next_begin_idx = *next_begin_idx + n + crlf.len();
-                            Ok(resp::Value::BulkStr(s.to_string()))
-                        } else {
-                            Err("not enough str")
-                        }
-                    }
-                    '*' => {
-                        let mut size = parse_str_to_num::<usize>(rest)?;
-                        let mut res_arr: Vec<resp::Value> = vec![];
-                        while size > 0 {
-                            let item =
-                                parse_content(raw_msg, *next_begin_idx, next_begin_idx)?;
-                            res_arr.push(item);
-                            size = size - 1;
-                        }
-                        Ok(resp::Value::Array(res_arr))
-                    },
-                    '%' => {
-                        let mut length = parse_str_to_num::<usize>(rest)?;
-                        let mut res_map: HashMap<resp::Value, resp::Value> = HashMap::new();
-
-                        while length > 0 {
-                            let k = parse_content(raw_msg, *next_begin_idx, next_begin_idx)?;
-                            let v = parse_content(raw_msg, *next_begin_idx, next_begin_idx)?;
-                            res_map.insert(k, v);
-                            length = length - 1;
-                        }
-
-                        Ok(resp::Value::Map(res_map))
-                    }
-                    _ => Err("unknow type"),
-                };
-            } else {
-                Err("empty str")
-            }
-        } else {
-            Err("missing CRLF")
-        }
-    } else {
-        Err("begin_idx out of range")
-    }
-}
-
-#[allow(unused)]
-fn parse(msg: &str) -> Result<resp::Value, &'static str> {
-    parse_content(msg, 0, &mut 0)
-}
+mod net;
 
 #[cfg(test)]
 mod tests {
-    use crate::{resp, parse};
-    use std::{collections::HashMap, fs, vec};
-
-    #[test]
-    fn it_works() {
-        let msg = "+OK\r\n";
-        let res = msg
-            .find("\r\n")
-            .and_then(|i: usize| -> Option<&str> { msg.get(0..i) });
-        assert_eq!(res, Some("+OK"));
-    }
+    use crate::{resp, resp::unpack};
+    use std::{collections::{HashMap, HashSet}, fs, vec};
 
     #[test]
     fn parse_resp() {
         let bulk_str = "$5\r\nHello\r\n";
 
         assert_eq!(
-            parse(bulk_str),
+            unpack(bulk_str.to_string()),
             Ok(resp::Value::BulkStr(String::from("Hello")))
         );
 
         let utf8_str = String::from("一杯🧊\r\n美式");
         let bulk_str_utf8 = format!("${}\r\n{}\r\n", utf8_str.len(), utf8_str);
         assert_eq!(
-            parse(&bulk_str_utf8),
+            unpack(bulk_str_utf8.to_string()),
             Ok(resp::Value::BulkStr(String::from("一杯🧊\r\n美式")))
         );
 
         let arr_str = "*2\r\n$5\r\nHello\r\n$5\r\nWorld\r\n";
         assert_eq!(
-            parse(arr_str),
+            unpack(arr_str.to_string()),
             Ok(resp::Value::Array(vec![
                 resp::Value::BulkStr(String::from("Hello")),
                 resp::Value::BulkStr(String::from("World"))
@@ -133,7 +33,7 @@ mod tests {
 
         let array_str2 = "*2\r\n*2\r\n$5\r\nHello\r\n$5\r\nWorld\r\n$5\r\nRust!\r\n";
         assert_eq!(
-            parse(array_str2),
+            unpack(array_str2.to_string()),
             Ok(resp::Value::Array(vec![
                 resp::Value::Array(vec![
                     resp::Value::BulkStr(String::from("Hello")),
@@ -144,28 +44,35 @@ mod tests {
         );
 
         let number_str = ":100\r\n";
-        assert_eq!(parse(number_str), Ok(resp::Value::Integer(100)));
+        assert_eq!(unpack(number_str.to_string()), Ok(resp::Value::Integer(100)));
 
         let number_str = ":-99\r\n";
-        assert_eq!(parse(number_str), Ok(resp::Value::Integer(-99)));
+        assert_eq!(unpack(number_str.to_string()), Ok(resp::Value::Integer(-99)));
 
         let number_str = ":+98\r\n";
-        assert_eq!(parse(number_str), Ok(resp::Value::Integer(98)));
+        assert_eq!(unpack(number_str.to_string()), Ok(resp::Value::Integer(98)));
 
         let map_str = "%5\r\n+name\r\n+xiamingjie\r\n$5\r\nhello\r\n$5\r\nworld\r\n:1\r\n+1\r\n+array\r\n*1\r\n+item1\r\n+map\r\n%1\r\n+map_k\r\n+map_v\r\n";
-        assert_eq!(parse(map_str), Ok(resp::Value::Map(HashMap::from([
+        assert_eq!(unpack(map_str.to_string()), Ok(resp::Value::Map(HashMap::from([
             (resp::Value::SimpleStr(String::from("name")), resp::Value::SimpleStr(String::from("xiamingjie"))),
             (resp::Value::BulkStr(String::from("hello")), resp::Value::BulkStr(String::from("world"))),
             (resp::Value::Integer(1), resp::Value::SimpleStr(String::from("1"))),
             (resp::Value::SimpleStr(String::from("array")), resp::Value::Array(vec![resp::Value::SimpleStr(String::from("item1"))])),
             (resp::Value::SimpleStr(String::from("map")), resp::Value::Map(HashMap::from([(resp::Value::SimpleStr(String::from("map_k")), resp::Value::SimpleStr(String::from("map_v")))]))),
         ]))));
+
+        let bool_str = "#t\r\n";
+        assert_eq!(unpack(bool_str.to_string()), Ok(resp::Value::Bool(true)));
+        let bool_str = "#f\r\n";
+        assert_eq!(unpack(bool_str.to_string()), Ok(resp::Value::Bool(false)));
+
+        assert_eq!(unpack("~2\r\n:1\r\n+xiamingjie\r\n".to_string()), Ok(resp::Value::Set(HashSet::from([resp::Value::Integer(1), resp::Value::SimpleStr("xiamingjie".to_string())]))));
     }
 
     #[test]
     fn parse_long_resp_example() {
         let content = fs::read_to_string("resp_example").expect("open the file failed");
-        let r: usize = match parse(&content.to_string()) {
+        let r: usize = match unpack(content) {
             Ok(res) => {
                 if let resp::Value::Array(d) = res {
                     d.len()
@@ -176,5 +83,17 @@ mod tests {
             Err(_) => 0
         };
         assert_eq!(r, 240);
+    }
+
+    #[test]
+    fn pack() {
+        assert_eq!(resp::pack(resp::Value::SimpleStr(String::from("hello"))), "+hello\r\n");
+        assert_eq!(resp::pack(resp::Value::Error(String::from("Error: redis nil"))), "-Error: redis nil\r\n");
+        assert_eq!(resp::pack(resp::Value::BulkStr(String::from("hello"))), "$5\r\nhello\r\n");
+        assert_eq!(resp::pack(resp::Value::Integer(99999)), ":99999\r\n");
+        assert_eq!(resp::pack(resp::Value::Bool(true)), "#t\r\n");
+        assert_eq!(resp::pack(resp::Value::Array(vec![resp::Value::BulkStr(String::from("hello")), resp::Value::Integer(123456)])), "*2\r\n$5\r\nhello\r\n:123456\r\n");
+        assert_eq!(resp::pack(resp::Value::Map(HashMap::from([(resp::Value::SimpleStr(String::from("abc")), resp::Value::Array(vec![resp::Value::BulkStr(String::from("hello")), resp::Value::Integer(123456)]))]))), "%1\r\n+abc\r\n*2\r\n$5\r\nhello\r\n:123456\r\n");
+        // assert_eq!(resp::pack(resp::Value::Set(HashSet::from([resp::Value::Integer(1), resp::Value::SimpleStr(String::from("xiamingjie"))]))), "~2\r\n:1\r\n+xiamingjie\r\n");
     }
 }
--- src/resp.rs
+++ src/resp.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, hash::{Hash, Hasher}};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::{Hash, Hasher},
+    str::FromStr,
+};
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Value {
@@ -8,20 +12,165 @@ pub enum Value {
     Array(Vec<Value>),
     BulkStr(String),
     Bool(bool),
-    Map(HashMap<Value, Value>)
+    Map(HashMap<Value, Value>),
+    Set(HashSet<Value>)
 }
 
 impl Hash for Value {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        // todo 正确性？
         match self {
-            Value::SimpleStr(_) => 0.hash(state),
-            Value::Error(_) => 1.hash(state),
-            Value::Integer(_) => 2.hash(state),
-            Value::Array(_) => 3.hash(state),
-            Value::BulkStr(_) => 4.hash(state),
-            Value::Bool(_) => 5.hash(state),
-            Value::Map(_) => 6.hash(state),
+            Value::SimpleStr(s) => s.hash(state),
+            Value::Error(e) => e.hash(state),
+            Value::Integer(n) => n.hash(state),
+            Value::Array(a) => a.hash(state),
+            Value::BulkStr(s) => s.hash(state),
+            Value::Bool(b) => b.hash(state),
+            Value::Map(m) => {
+                for (k, v) in m {
+                    k.hash(state);
+                    v.hash(state);
+                }
+            }
+            Value::Set(s) => {
+                for e in s {
+                    e.hash(state)
+                }
+            }
+        }
+    }
+}
+
+fn parse_str_to_num<T>(s: &str) -> Result<T, &'static str>
+where
+    T: FromStr,
+{
+    if let Some(n) = s.parse::<T>().ok() {
+        Ok(n)
+    } else {
+        Err("parse str to number failed")
+    }
+}
+
+#[allow(unused)]
+fn parse(
+    raw_msg: &str,
+    begin_idx: usize,
+    next_begin_idx: &mut usize,
+) -> Result<Value, &'static str> {
+    // todo need more specific error info
+    let crlf = "\r\n";
+    if let Some(msg_slice) = raw_msg.get(begin_idx..) {
+        if let Some(body) = msg_slice
+            .find(crlf)
+            .and_then(|i| raw_msg.get(begin_idx..begin_idx + i))
+        {
+            *next_begin_idx = begin_idx + body.len() + crlf.len();
+            if let Some((first_char, rest)) =
+                body.chars().next().map(|c| (c, &body[c.len_utf8()..]))
+            {
+                return match first_char {
+                    '+' => Ok(Value::SimpleStr(rest.to_string())),
+                    '-' => Ok(Value::Error(rest.to_string())),
+                    ':' => {
+                        let n = parse_str_to_num::<i64>(rest)?;
+                        Ok(Value::Integer(n))
+                    }
+                    '#' => match rest {
+                        "t" => Ok(Value::Bool(true)),
+                        "f" => Ok(Value::Bool(false)),
+                        _ => Err("unknow type"),
+                    },
+                    '$' => {
+                        let n = parse_str_to_num::<usize>(rest)?;
+                        if let Some(s) = raw_msg.get(*next_begin_idx..*next_begin_idx + n) {
+                            *next_begin_idx = *next_begin_idx + n + crlf.len();
+                            Ok(Value::BulkStr(s.to_string()))
+                        } else {
+                            Err("not enough str")
+                        }
+                    }
+                    '*' => {
+                        let mut size = parse_str_to_num::<usize>(rest)?;
+                        let mut res_arr: Vec<Value> = vec![];
+                        while size > 0 {
+                            let item = parse(raw_msg, *next_begin_idx, next_begin_idx)?;
+                            res_arr.push(item);
+                            size = size - 1;
+                        }
+                        Ok(Value::Array(res_arr))
+                    }
+                    '%' => {
+                        let mut length = parse_str_to_num::<usize>(rest)?;
+                        let mut res_map: HashMap<Value, Value> = HashMap::new();
+
+                        while length > 0 {
+                            let k = parse(raw_msg, *next_begin_idx, next_begin_idx)?;
+                            let v = parse(raw_msg, *next_begin_idx, next_begin_idx)?;
+                            res_map.insert(k, v);
+                            length = length - 1;
+                        }
+
+                        Ok(Value::Map(res_map))
+                    }
+                    '~' => {
+                        let mut length = parse_str_to_num::<usize>(rest)?;
+                        let mut res_set: HashSet<Value> = HashSet::new();
+                        while length > 0 {
+                            res_set.insert(parse(raw_msg, *next_begin_idx, next_begin_idx)?);
+                            length = length - 1;
+                        }
+                        Ok(Value::Set(res_set))
+                    }
+                    _ => Err("unknow type"),
+                };
+            } else {
+                Err("empty str")
+            }
+        } else {
+            Err("missing CRLF")
+        }
+    } else {
+        Err("begin_idx out of range")
+    }
+}
+
+#[allow(unused)]
+pub fn unpack(msg: String) -> Result<Value, &'static str> {
+    parse(&msg, 0, &mut 0)
+}
+
+#[allow(unused)]
+pub fn pack(value: Value) -> String {
+    match value {
+        Value::SimpleStr(s) => format!("+{}\r\n", s),
+        Value::Error(s) => format!("-{}\r\n", s),
+        Value::BulkStr(s) => format!("${}\r\n{}\r\n", s.len(), s),
+        Value::Bool(b) => format!("#{}\r\n", if b {"t"} else {"f"}),
+        Value::Integer(n) => format!(":{}\r\n", n),
+        Value::Array(a) => {
+            let length = a.len();
+            let mut str = String::from("");
+            for item in a {
+                str.push_str(&pack(item));
+            }
+            format!("*{}\r\n{}", length, str)
+        }
+        Value::Map(m) => {
+            let length = m.len();
+            let mut str = String::from("");
+            for (k, v) in m {
+                str.push_str(&pack(k));
+                str.push_str(&pack(v));
+            }
+            format!("%{}\r\n{}", length, str)
+        }
+        Value::Set(s) => {
+            let length = s.len();
+            let mut str = String::from("");
+            for e in s {
+                str.push_str(&pack(e));
+            }
+            format!("~{}\r\n{}", length, str)
         }
     }
 }
